759136efcf282b58ed4bd93e70d58bd4d861ba2b
Rifqi <rifqimhm@gmail.com>
2021-04-01 13:08:31 +0700

    Create run_wordpress_with_persistent_volume_and_published_port.sh

---
+++ run_wordpress_with_persistent_volume_and_published_port.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker container run -d \
+--name wordpress \
+-e WORDPRESS_DB_HOST=wordpressdb \
+-e WORDPRESS_DB_USER='wordpress' \
+-e WORDPRESS_DB_PASSWORD='redhat' \
+-e WORDPRESS_DB_NAME='wordpress' \
+-v /root/var/www/html:/var/www/html \
+--network mariadb-wp-privnet \
+-p 80:80 \
+wordpress
